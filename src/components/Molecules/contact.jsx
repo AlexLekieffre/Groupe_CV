1eb74559e7040b71e7b749e5e9b7c3591982793e
Eloise Huet (alias: Hergie) <Hergie59410@gmail.com>
2022-06-16 13:59:12 +0200

    ajout des lien pour les bouttons vers Facebook/Twitter/LinkedIn

--- src/components/Molecules/contact.jsx
+++ src/components/Molecules/contact.jsx
@@ -3,9 +3,9 @@ import {FaFacebook, FaTwitter, FaLinkedin} from 'react-icons/fa'
 const Contact = () => (
     <div>
         <h1 className="heading">CONTACTEZ-NOUS</h1>
-        <button><FaFacebook/></button>
-        <button><FaTwitter/></button>
-        <button><FaLinkedin/></button>
+        <button><a href='https://www.facebook.com/'><FaFacebook/></a></button>
+        <button><a href='https://twitter.com/'><FaTwitter/></a></button>
+        <button><a href='https://www.linkedin.com/'><FaLinkedin/></a></button>
         <br />
         <form>
             <label>VOTRE PRENOM</label>
