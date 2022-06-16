a26f371cc54f67f9b573f8d749f7bfc57b1276cd
Adeline R <adeline.rdev@gmail.com>
2022-06-16 14:27:29 +0200

    alignment updates

--- src/components/Molecules/contact.jsx
+++ src/components/Molecules/contact.jsx
@@ -1,31 +1,43 @@
-import {FaFacebook, FaTwitter, FaLinkedin} from 'react-icons/fa'
+import { FaFacebook, FaTwitter, FaLinkedin } from 'react-icons/fa'
 
 const Contact = () => (
-    <div>
-        <h1 className="heading">CONTACTEZ-NOUS</h1>
-        <button><a href='https://www.facebook.com/'><FaFacebook/></a></button>
-        <button><a href='https://twitter.com/'><FaTwitter/></a></button>
-        <button><a href='https://www.linkedin.com/'><FaLinkedin/></a></button>
-        <br />
-        <form>
-            <label>VOTRE PRENOM</label>
-            <input type="text"/>
-            <br />
-            <label>VOTRE NOM</label>
-            <input type="text"/>
-            <br />
-            <label>VOTRE EMAIL</label>
-            <input type="text" />
-            <br />
-            <label>VOTRE NUMERO DE TELEPHONE</label>
-            <input type="text" />
-            <br />
-            <label>VOTRE MESSAGE</label>
-            <textarea type="text"></textarea>
-            <br />
-            <button type="submit">ENVOYEZ</button>
-        </form>
-    </div>
-  )
-  
-  export default Contact;
+  <div>
+    <h1 className="heading">CONTACTEZ-NOUS</h1>
+    <button>
+      <a href="https://www.facebook.com/">
+        <FaFacebook />
+      </a>
+    </button>
+    <button>
+      <a href="https://twitter.com/">
+        <FaTwitter />
+      </a>
+    </button>
+    <button>
+      <a href="https://www.linkedin.com/">
+        <FaLinkedin />
+      </a>
+    </button>
+    <br />
+    <form>
+      <label>VOTRE PRENOM</label>
+      <input type="text" />
+      <br />
+      <label>VOTRE NOM</label>
+      <input type="text" />
+      <br />
+      <label>VOTRE EMAIL</label>
+      <input type="text" />
+      <br />
+      <label>VOTRE NUMERO DE TELEPHONE</label>
+      <input type="text" />
+      <br />
+      <label>VOTRE MESSAGE</label>
+      <textarea type="text"></textarea>
+      <br />
+      <button type="submit">ENVOYEZ</button>
+    </form>
+  </div>
+)
+
+export default Contact
--- src/components/Molecules/experience.jsx
+++ src/components/Molecules/experience.jsx
@@ -4,11 +4,16 @@ const Experience = () => {
       <div>
         <img src="/src/img/banner.png" alt="" />
       </div>
+
       <h1>Mes Expériences </h1>
       <hr />
-      ➪ 2 ans chez Mcdo <br />
-      ➪ 8 ans à la Nasa en tant qu'astronaute remplacant <br />
-      ➪ 3 ans Web Dev sur le dark web <br />
+
+      <p>
+        ➪ 2 ans chez Mcdo <br />
+        ➪ 8 ans à la Nasa en tant qu'astronaute remplacant <br />
+        ➪ 3 ans Web Dev sur le dark web <br />
+      </p>
+
       {/* <ul>
         <li> 2 ans chez Mcdo </li>
         <li> 8 ans à la Nasa en tant qu'astronaute remplacant</li>
