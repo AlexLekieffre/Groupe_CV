b35dfecf7a079c8fd65cfdb1c6babc6a9f7da73d
Walid Chtairi <walid.chtairi@labom2iformation.fr>
2022-06-16 14:50:31 +0200

    update Team Component, package.json and App.js

--- src/components/Molecules/team.jsx
+++ src/components/Molecules/team.jsx
@@ -1,24 +1,55 @@
 
+import styled from "styled-components";
 import { useState } from "react";
 
-const Team = () => {
-  const [teamMemberAlex, setTeamMemberAlex]= useState({
-    firstname : "Alexandre",
-    name : "Lekieffre",
-    function : "Baby dev",
-    experience1 : "Responsable Technique",
-    experience2 : "Chef de chantier en industrie",
-    experience3 : "Electrotechniciens"
-    
-})
 
-console.log(teamMemberAlex);
-  return (
-    <div className="team">
-      {[teamMemberAlex.firstname,teamMemberAlex.name,teamMemberAlex.function].join(" ")}
-    </div>
+const div  = styled.div`
+color: blue;
+`
+
+function Team () {
+
+  const [name] = useState ('Name');
+  const [job] = useState ('Job');
+  const [about] = useState ('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip');
+
+
+  return(
+      <div className="card">
+          <div className="card_uppercontainer">
+              <div classname="image_container">
+                  <img src="https://cdn.pixabay.com/photo/2022/06/04/18/53/fern-7242590_960_720.jpg" alt="feuille" height="100px" width="100px" />
+              </div>
+          </div>
+          <div className="Card_lowercontainer">
+              <h3> { name } </h3>
+              <h3> { job } </h3>
+              <p> { about } </p>
+          </div>
+       </div>                
   );
-};
+}
 
 
-export default Team;
+
+
+  // import { useState } from "react";
+
+  // const Team = () => {
+  //   const [teamMemberAlex, setTeamMemberAlex]= useState({
+  //     firstname : "Alexandre",
+  //     name : "Lekieffre",
+  //     function : "Baby dev",
+  //     toto : "ok"
+  // })
+
+  // console.log(teamMemberAlex);
+  //   return (
+  //     <div className="team">
+  //       {[teamMemberAlex.firstname,teamMemberAlex.name,teamMemberAlex.function].join(" ")}
+  //     </div>
+  //   );
+  // };
+
+
+  export default Team;
