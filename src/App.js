3b0f16c5d414dbd1dd2eb6caa1557e90ad2bd807
Adeline R <adeline.rdev@gmail.com>
2022-06-20 14:05:42 +0200

    Ok, ALL merge problems are fixed now

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-<<<<<<< HEAD
-import "./App.css";
-/* import Team from "./components/molecules/team"; */
-import CV from "./containers/CV";
-=======
 
 import './App.css';
 import CV from './containers/CV';
 import Team from './components/Molecules/team';
->>>>>>> ef64d1b (update Team Component, package.json and App.js)
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
-      <CV></CV>
-=======
 
       <CV ><Team /></CV>
 
->>>>>>> ef64d1b (update Team Component, package.json and App.js)
     </div>
   );
 }
